5c7b1561c72f2a279c1df31af46f083d1f74b0b6
Anth0-12 <antho62680@live.fr>
2023-01-24 16:39:53 +0100

    formules

---
+++ js/slick_ocalme.js
@@ -0,0 +1,13 @@
+
+$('.slider_ocalme').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  prevArrow: false,
+    nextArrow: false
+});
+
+$(".slick-next").text("Suivant");
+
+$(".slick-prev").text("Précédent");
