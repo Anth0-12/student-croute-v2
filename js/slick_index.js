b7adebec657f011d2781a1c5aded9ee8ecd3c27e
Anth0-12 <antho62680@live.fr>
2023-01-29 18:04:28 +0100

    formules + carousel

--- js/slick_index.js
+++ js/slick_index.js
@@ -1,17 +1,17 @@
 
-// $('.slider').slick({
-//   rows: 2,
-//   slidesToShow: 3,
-//   slidesToScroll: 3,
-//   responsive: [
-//       {
-//       breakpoint: 768,
-//       settings: {
-//          slidesToShow: 1           
-//       }
-//      }
-//    ]            
-//  }); 
+$('.container').slick({
+  rows: 2,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+      {
+      breakpoint: 768,
+      settings: {
+         slidesToShow: 1           
+      }
+     }
+   ]            
+ }); 
 
 // $(".slick-next").text("Suivant");
 
