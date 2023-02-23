12929f74511da6dd36844f1e3014fd110386ed0a
Anth0-12 <antho62680@live.fr>
2023-02-23 20:16:50 +0100

    responsive

--- js/carousel_tel.js
+++ js/carousel_tel.js
@@ -2,10 +2,18 @@ const slider = document.querySelector("#slider");
 const sliderContainer = slider.querySelector(".slider-container");
 const images = sliderContainer.querySelectorAll("img");
 
+
+// var x = window.matchMedia("(max-width: 740px)")
+// var y = window.matchMedia("(max-width: 600px)")
+
 let index = 0;
 
 function changeSlide() {
-  sliderContainer.style.left = `-${index * 100}%`;
+  
+    sliderContainer.style.left = `-${index * 100}%`;
+  
+
+  
 }
 
 setInterval(() => {
